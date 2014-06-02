61a2b8c2a6e8df10bf2f273ba2acdb8c085e418b
Zoltan Simari <simari.zoltan@gmail.com>
2014-06-02 20:21:56 +0100

    Changes for the the first three points

--- app/scripts/orders.js
+++ app/scripts/orders.js
@@ -1,28 +1,91 @@
 (function () {
-  'use strict';
-  /* global _, foodit */
-  foodit.orders = {
+    'use strict';
+    /* global _, foodit */
+    foodit.orders = {
 
-    Model: foodit.Model.extend({
-      idAttribute: 'orderId',
-    }),
+        Order: foodit.Model.extend({
+            idAttribute: 'orderId'
+        }),
 
-    Collection: foodit.Collection.extend({
-      urlRoot: '/data/orders-',
-      url: function () {
-        return this.urlRoot + this.id + '.json'
-      }
-    }),
+        Collection: foodit.Collection.extend({
+            initialize: function(options) {
+                this.id = options.restaurant;
+            },
+            urlRoot: '/data/orders-',
+            url: function () {
+                return this.urlRoot + this.id + '.json'
+            },
+            sortDirection: 1,
+            sortProperty: '',
+            strategies: {
+                Date: function (order) { return this.sortDirection * new Date(order.get('created')); },
+                Total: function (order) { return this.sortDirection * order.get('totalValue'); }
+            },
+            changeSort: function (sortProperty) {
+                var strategy = this.strategies[sortProperty];
+                if (strategy !== undefined) {
+                    this.sortDirection = strategy === this.comparator ? this.sortDirection * -1 : 1;
+                    this.sortProperty = sortProperty;
+                    this.comparator = strategy;
+                    this.sort();
+                    return true;
+                }
+            }
+        }),
 
-    View: foodit.View.extend({
-      template: _.template($('#t-orders').html()),
-      initialize: function () {
-        this.restaurantSelect = new foodit.restaurants.SelectView
-      }
+        ContainerView: foodit.View.extend({
+            template: _.template($('#t-order-container').html()),
+            initialize: function () {
+                this.orderView = new foodit.orders.OrderView;
+                this.restaurantSelect = new foodit.restaurants.SelectView;
+                this.listenTo(this.restaurantSelect, 'change:restaurant', function (restaurantId) {
+                    var _this = this;
+                    this.orderView.collection = new foodit.orders.Collection({restaurant: restaurantId});
+                    this.orderView.collection.fetch({
+                        error: function () {
+                            console.error('failed to fetch orders :(')
+                        },
+                        success: function () {
+                            _this.orderView.render()
+                        }
+                    });
+                })
+            },
+            render: function () {
+                this.$el.html(this.template());
+                this.restaurantSelect.render();
+                this.orderView.render();
+                this.$el.find('#restaurants-select-group').append(this.restaurantSelect.el);
+                this.$el.append(this.orderView.el);
+                return this;
+            },
+            events: {
+                'click th': 'headerClick'
+            },
+            headerClick: function( e ) {
+                var $el = $(e.currentTarget);
+                var success = this.orderView.collection.changeSort($el.text());
+
+                if (success === true) {
+                    this.orderView.render();
+                }
+            }
+        }),
+
+        OrderView: foodit.View.extend({
+            template: _.template($('#t-orders').html()),
+            render: function () {
+                this.$el.html(this.template({orders: this.collection ? this.collection.toJSON() : null}))
+
+                if (this.collection !== undefined && this.collection.sortProperty !== '') {
+                    this.$el.find('th:contains(' + this.collection.sortProperty + ')')
+                        .addClass(this.collection.sortDirection === 1 ? 'sorted-column-asc' : 'sorted-column-desc');
+                }
+            }
+        })
+
+    }
+    foodit.router.route('orders', 'orders', function () {
+        this.setView(new foodit.orders.ContainerView)
     })
-
-  }
-  foodit.router.route('orders', 'orders', function () {
-    this.setView(new foodit.orders.View)
-  })
 })()
